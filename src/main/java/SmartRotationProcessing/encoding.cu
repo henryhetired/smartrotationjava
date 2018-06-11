798c9d6455e26dc2b974ff7d1a44d0200baae591
henryhe5692wow@gmail.com <henryhe5692wow@gmail.com>
2018-06-11 18:29:42 -0500

    fixed

--- src/main/java/SmartRotationProcessing/encoding.cu
+++ src/main/java/SmartRotationProcessing/encoding.cu
@@ -1,14 +1,15 @@
 extern "C"
-__global__ void calc_entropy_atomic(float *float_image_in, float *entropy_out, int* blk_size) {
+__global__ void calc_entropy_atomic(float *float_image_in, float *entropy_out, int* blk) {
 	//TODO: CHECK INDEX FOR ENTROPY_OUT
 	//calculate entropy of a block through a single thread
+        int blk_size = &blk;
 	__shared__ float sum;
 	if (threadIdx.x == 0 && threadIdx.y == 0) {
 		sum = 0.0;
 	}
 	__syncthreads();
 	__shared__ float c;
-	int blocksize = blk_size*blk_size;
+	int blocksize = &blk_size*&blk_size;
 	//vertical offset to get to beginning of own block
 	int v_offset_to_blkrow = gridDim.x*blockDim.x*blockDim.y*blockIdx.y;
 	int v_offset_to_pixrow = blockDim.x*gridDim.x*threadIdx.y;
@@ -33,9 +34,10 @@ __global__ void calc_entropy_atomic(float *float_image_in, float *entropy_out, i
 		entropy_out[out_idx] = entropy;
 	}
 }
-__global__ void thread_dct_h(float *float_image_in, float *coefficients, float *float_image_out, int* blk_size) {
+__global__ void thread_dct_h(float *float_image_in, float *coefficients, float *float_image_out, int* blk) {
 	//dct on rows
 	//summation using Kahan algorithm, very important!
+  int blk_size = &blk;
 	float sum = 0.0;
 	float c = 0.0;
 	for (int i = 0; i<blk_size; i++) {
@@ -49,9 +51,10 @@ __global__ void thread_dct_h(float *float_image_in, float *coefficients, float *
 	}
 	float_image_out[gridDim.x*blockIdx.y*blockDim.x*blockDim.y + threadIdx.y*gridDim.x*blockDim.x + threadIdx.x + blockIdx.x*blockDim.x] = sum;
 }
-__global__ void thread_dct_v(float *float_image_in, float *coefficients, float *float_image_out, int* blk_size) {
+__global__ void thread_dct_v(float *float_image_in, float *coefficients, float *float_image_out, int* blk) {
 	// dct on columns
 	//summation using Kahan algorithm, very important!
+  int blk_size = &blk;
 	float sum = 0.0;
 	float c = 0.0;
 	for (int i = 0; i<blk_size; i++) {
